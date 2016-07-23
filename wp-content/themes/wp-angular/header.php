863af60563e43659a21692f8ec7bcc187514efba
Guille <nercof@github.com>
2016-07-23 20:43:41 -0300

    Reemplazo state por router. Actualizo package.json

--- wp-content/themes/wp-angular/header.php
+++ wp-content/themes/wp-angular/header.php
@@ -19,7 +19,9 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
+<!-- Inicio: Incorporamos los script_name -->
+<!-- Fin: -->   
 </head>
 
 <body <?php body_class(); ?>>
