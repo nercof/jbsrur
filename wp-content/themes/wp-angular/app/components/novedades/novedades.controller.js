9c51f993607fa0e0052668d9a697873db3ee4b72
Guille <nercof@github.com>
2016-09-27 22:09:42 -0300

    Add novedades slider

--- wp-content/themes/wp-angular/app/components/novedades/novedades.controller.js
+++ wp-content/themes/wp-angular/app/components/novedades/novedades.controller.js
@@ -2,16 +2,16 @@
     'use strict';
 
     angular
-        .module('app.core')
-        .controller('novedadesDestacadasController', novedadesDestacadasController);
+    .module('app.core')
+    .controller('novedadesDestacadasController', novedadesDestacadasController);
 
-    novedadesDestacadasController.$inject = ['postFactory', '$scope'];
+    novedadesDestacadasController.$inject = ['postFactory', '$scope', 'mediaFactory'];
 
     /**
-     * novedadesController: Gestión de últimos post con category:Novedades.
-     *  - @view: content
-     */
-    function novedadesDestacadasController(postFactory, $scope) {
+    * novedadesController: Gestión de últimos post con category:Novedades.
+    *  - @view: content
+    */
+    function novedadesDestacadasController(postFactory, $scope, mediaFactory) {
         var vm = this;
         vm.novedades = {};
         vm.slides = [];
@@ -20,20 +20,20 @@
         create();
 
         /**
-         * create() Coleccion de Novedades del sitio.
-         *
-         * Se instancia desde:
-         *  - menú Novedades:
-         *    Muestra el detalle de los últimos post con category:novedades.
-         *
-         * Atributos a tener en cuenta:
-         *  {object.novedad}.excerpt.rendered: descripciones de un post
-         *  {object.novedad}.title.rendered:The title for the object
-         *  {object.novedad}.author (?)
-         *  {object.novedad}.format: {standard, video, post, ...}
-         *  {object.novedad}.tags: {5:destacados, 6:otros} -> @see:http://v2.wp-api.org/reference/tags/
-         *  {object.novedad}.featured_media: que se tiene que buscar en {object.novedad}.guid.rendered
-         */
+        * create() Coleccion de Novedades del sitio.
+        *
+        * Se instancia desde:
+        *  - menú Novedades:
+        *    Muestra el detalle de los últimos post con category:novedades.
+        *
+        * Atributos a tener en cuenta:
+        *  {object.novedad}.excerpt.rendered: descripciones de un post
+        *  {object.novedad}.title.rendered:The title for the object
+        *  {object.novedad}.author (?)
+        *  {object.novedad}.format: {standard, video, post, ...}
+        *  {object.novedad}.tags: {5:destacados, 6:otros} -> @see:http://v2.wp-api.org/reference/tags/
+        *  {object.novedad}.featured_media: que se tiene que buscar en {object.novedad}.guid.rendered
+        */
         function create() {
             // Buscamos las novedades.
             postFactory.getPostByCategoryName("novedades").then(function(data) {
@@ -50,12 +50,22 @@
                 _.each(vm.novedades, function(novedad, i){
                     var actived = (i == 0 ? true : false );
                     novedad.active = actived;
+
+                    // Buscamos la imagen relacionada
+                    mediaFactory.getMedia(novedad.featured_media).then(function(data) {
+                        novedad.foto = data;
+                        if (!_.isEmpty(novedad.foto.guid)) {
+                            novedad.url = novedad.foto.guid.rendered;
+                        }
+
+                    });
+
                     if(i % 4 == 0) {
                         // creamos slides de 4 novedades
                         vm.slides.push( vm.novedades.slice(i, i + 4) );
                     }
                 })
-                console.log(vm.slides, vm.iconos_format);
+                //console.log(vm.slides, vm.iconos_format);
             });
         }
     }
