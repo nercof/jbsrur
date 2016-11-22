ff6a1206040bf7e16a62062b28054475f6faa0c5
Guille <nercof@github.com>
2016-11-22 00:59:06 -0300

    Add sucursales

--- wp-content/themes/wp-angular/app/components/suc-section/suc-section.controller.js
+++ wp-content/themes/wp-angular/app/components/suc-section/suc-section.controller.js
@@ -2,27 +2,50 @@
     'use strict';
 
     angular
-        .module('app.core')
-        .controller('sucSectionController', sucSectionController);
+    .module('app.core')
+    .controller('sucSectionController', sucSectionController);
 
-    sucSectionController.$inject = ['typeFactory'];
+    sucSectionController.$inject = ['typeFactory', 'mediaFactory'];
 
     /* @ngInject */
-    function sucSectionController(typeFactory) {
+    function sucSectionController(typeFactory, mediaFactory) {
         var vm = this;
+        vm.img = [];
 
         activate();
 
+        /*
+        * Cargar sucursales e imagenes.
+        */
         function activate() {
             console.log("<< sucSectionController >>");
 
             typeFactory.getSucursales().then(
                 function(data){
                     vm.sucursales = data;
-                    console.log("sucSectionController");
+
+                    // Cargar la url de la imagen
+                    setImages();
+                    console.log(" getSucursales() ");
                     console.log(vm.sucursales);
+
                 }
             );
+        }
+
+        function setImages() {
+            var medias = [];
+
+            // Recorremos las sucursales y obtenemos los id.media
+            _.each(vm.sucursales, function (sucursal) {
+
+                // Buscamos las imagenes en la WP
+                mediaFactory.getMedia(sucursal.featured_media).then(function(data){
+                    sucursal.img = data.source_url;
+                    console.log(sucursal.img);
+                });
+            });
+
         }
     }
 })();
