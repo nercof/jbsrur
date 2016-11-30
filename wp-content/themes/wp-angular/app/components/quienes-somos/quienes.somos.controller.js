cbd5e5b34c88ae6f5193f178e1ca0983ef94cdab
Guille <nercof@github.com>
2016-11-30 19:47:39 -0300

    BE QuienesSomos

--- wp-content/themes/wp-angular/app/components/quienes-somos/quienes.somos.controller.js
+++ wp-content/themes/wp-angular/app/components/quienes-somos/quienes.somos.controller.js
@@ -8,11 +8,12 @@
     /**
     * quienesSomosController:
     */
-    function quienesSomosController($scope, typeFactory) {
+    function quienesSomosController($scope, typeFactory, mediaFactory) {
         var vm = this;
 
         vm.title_view = '';
-        vm.qSomos = {}
+        vm.qSomos = {};
+        vm.autores = [];
 
         // Activamos
         activate(vm);
@@ -25,10 +26,45 @@
         function activate(vm) {
             getQuienesSomos().then(function (data){
                 vm.qSomos = data;
-                console.log(vm.qSomos);
+
+                typeFactory.getPostsByContentType("autor").then(function(data) {
+                    var autores_all = data;
+
+                    // Formateamos los autores
+                    var slug = vm.qSomos["wpcf-miembros"].split(",");
+
+                    // Recorremos qSomos
+                    _.each(slug, function (miembro) {
+                        _.find(autores_all, function(autor){
+                            // Verificamos el autor
+                            if (autor.slug == miembro.trim()) {
+                                // Incorporamos el autor
+                                vm.autores.push(autor);
+                            }
+                        });
+                    });
+                    // Tenemos que hacer el link de media
+                    setImages();
+                    console.log(vm.autores);
+                    console.log(vm.qSomos);
+                });
             });
         } // fin activate()
 
+        /**
+        * Permite mapear el id de imagen con la url del BE.
+        */
+        function setImages() {
+            var medias = [];
+
+            // Recorremos las sucursales y obtenemos los id.media
+            _.each(vm.autores, function (autor) {
+                // Buscamos las imagenes en WP
+                mediaFactory.getMedia(autor.featured_media).then(function(data){
+                    autor.image = data.source_url;
+                });
+            });
+        }
         /**
         * Obtener los <emprendimientos> desde la API de Tokko
         * @param {} page - <description>
